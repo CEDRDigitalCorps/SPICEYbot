d9de1e11706910e06546fe2cb631753908b7311b
Isaiah Zwick-Schachter <isaiah@developingtechnician.com>
2017-09-10 21:53:53 -0400

    Fixed script

--- slack.py
+++ slack.py
@@ -4,11 +4,32 @@ import os
 slack_token = os.environ['SLACK_TOKEN']
 sc = SlackClient(slack_token)
 
-def say(msg, chan):
+def get_reacts(msg):
+    return sc.api_call(
+      "reactions.get",
+      full="true",
+      channel="C70127ZL1",
+      timestamp=msg
+    )['message'].get('reactions', [])
+
+def smart():
+    messages = sc.api_call(
+      "search.all",
+      query="from:TweetBot"
+    )['messages']['matches']
+    for i in messages:
+        print(i['text'])
+        reacts = get_reacts(i['ts'])
+        for a in reacts:
+            print("Got "+str(a['count'])+" "+str(a['name'])+"s")
+
+
+def say(msg):
     print(sc.api_call(
       "chat.postMessage",
-      channel=chan,
+      channel="#twitterautosearch",
       text=msg
     ))
 
-say("I'm so glad to finally be alive!", "#twitterautosearch")
+smart()
+#say("hi")
