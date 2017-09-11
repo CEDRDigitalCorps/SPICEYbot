e2961b4ddbc134b09544b02f31afdb54628f27c8
Greg Reinbach <greg@reinbach.com>
2017-09-11 16:11:23 -0400

    Refactor db interface with settings integration

--- database_interface/database_interface.py
+++ database_interface/database_interface.py
@@ -1,95 +1,70 @@
+import json
 import psycopg2
 import sys
 
+from decouple import config
+
+
 class DatabaseInterface:
     def __init__(self):
-        self.database_password = 'ENV PASSWORD HERE'
-        self.database_name = 'spicey'
-        self.table_name = 'messages'
-        self.user = 'spicey'
+        self.database_host = config("DATABASE_HOST", default="")
+        self.database_password = config("DATABASE_PASSWORD", default="")
+        self.database_name = config("DATABASE_NAME", default="spicey")
+        self.table_name = config("DATABASE_TABLE", default="messages")
+        self.user = config("DATABASE_USER", default="spicey")
 
-    def create_message_table(self):
+    def db_execute(self, query):
+        print("Execute: {}".format(query))
         con = None
         try:
-            con = psycopg2.connect(database=self.database_name, user=self.user, password=self.database_password)
+            con = psycopg2.connect(
+                host=self.database_host,
+                database=self.database_name,
+                user=self.user,
+                password=self.database_password
+            )
+            print("Have db connection")
             cur = con.cursor()
-            cur.execute("CREATE TABLE test (id serial PRIMARY KEY, url varchar(100), body text, entered boolean, discarded boolean, data json);")
+            cur.execute(query)
             con.commit()
+            return cur
 
         except psycopg2.DatabaseError, e:
             print 'Error %s' % e
+            if con:
+                print("Rolling back...")
+                con.rollback()
             sys.exit(1)
 
         finally:
             if con:
                 con.close()
+
+    def create_message_table(self):
+        self.db_execute("CREATE TABLE {} (id serial PRIMARY KEY, url varchar(100), body text, entered boolean, discarded boolean, data json);".format(self.table_name))
 
     def _stringify_dict(d):
         return '{%s}' % ', '.join(['"%s": "%s"' % (k, v) for k, v in d.items()])
 
     def add(self, msg_json):
-        con = None
-        table_name = 'test'
-        try:
-
-            con = psycopg2.connect("dbname='scraper' user='andy'")
-            cur = con.cursor()
-            query = "INSERT INTO {0} VALUES (nextval('{0}_id_seq'),'{1}', '{2}', '{3}', '{4}', '{5}')".format(
-                table_name,
-                msg_dict['url'],
-                msg_dict['body'],
-                msg_dict['entered'],
-                msg_dict['discarded'],
-                self._stringify_dict(msg_dict['data'])
-            )
-            cur.execute(query)
-            con.commit()
-
-        except psycopg2.DatabaseError, e:
-            if con:
-                con.rollback()
-
-            print 'Error %s' % e
-            sys.exit(1)
-
-        finally:
-            if con:
-                con.close()
-
+        msg_dict = json.loads(msg_json)
+        self.db_execute("INSERT INTO {0} VALUES ('{1}', '{2}', '{3}', '{4}', '{5}')".format(
+            self.table_name,
+            msg_dict['url'],
+            msg_dict['body'],
+            msg_dict['entered'],
+            msg_dict['discarded'],
+            self._stringify_dict(msg_dict['data'])
+        ))
 
     def get(self, url):
-        con = None
-        try:
-            con = psycopg2.connect("dbname='scraper' user='andy'")
-            cur = con.cursor()
-            cur.execute("SELECT * FROM test WHERE url={0};".forma(url))
+        cur = self.db_execute("SELECT * FROM test WHERE url={0};".forma(url))
+        if cur is not None:
             return cur.fetchone()
-
-        except psycopg2.DatabaseError, e:
-            if con:
-                con.rollback()
-            print 'Error %s' % e
-            sys.exit(1)
-
-        finally:
-            if con:
-                con.close()
+        return None
 
     def exists(self, url):
-        con = None
-        try:
-            con = psycopg2.connect("dbname='scraper' user='andy'")
-            cur = con.cursor()
-            cur.execute("SELECT * FROM test WHERE url={0};".format(url))
-            if cur.fetchone(): return True
-            return False
-
-        except psycopg2.DatabaseError, e:
-            if con:
-                con.rollback()
-            print 'Error %s' % e
-            sys.exit(1)
-
-        finally:
-            if con:
-                con.close()
+        cur = self.db_execute("SELECT * FROM test WHERE url={0};".format(url))
+        if cur is not None and cur.fetchone():
+            return True
+        return False
