879927f53bb0120c51b67540994f6710ff3fe850
Isaiah Zwick-Schachter <isaiah@developingtechnician.com>
2017-09-10 22:21:00 -0400

    Finished slack bot

--- slack.py
+++ slack.py
@@ -4,32 +4,30 @@ import os
 slack_token = os.environ['SLACK_TOKEN']
 sc = SlackClient(slack_token)
 
-def get_reacts(msg):
-    return sc.api_call(
-      "reactions.get",
-      full="true",
-      channel="C70127ZL1",
-      timestamp=msg
-    )['message'].get('reactions', [])
+class LogicProc:
+    def __init__(self):
+        pass
+    def post_to_slack(self, msg):
+        sc.api_call(
+          "chat.postMessage",
+          channel="#twitterautosearch",
+          text=msg
+        )
+    def poll_slack_reactions(self):
+        new_reacts_thing = {}
+        messages = sc.api_call(
+          "search.all",
+          query="from:TweetBot"
+        )['messages']['matches']
+        for i in messages:
+            reacts = sc.api_call(
+              "reactions.get",
+              full="true",
+              channel="C70127ZL1",
+              timestamp=i['ts']
+            )['message'].get('reactions', [])
+            new_reacts_thing[i['ts']] = []
+            for a in reacts:
+                new_reacts_thing[i['ts']].append({a['name']: a['count']})
+        return new_reacts_thing
 
-def smart():
-    messages = sc.api_call(
-      "search.all",
-      query="from:TweetBot"
-    )['messages']['matches']
-    for i in messages:
-        print(i['text'])
-        reacts = get_reacts(i['ts'])
-        for a in reacts:
-            print("Got "+str(a['count'])+" "+str(a['name'])+"s")
-
-
-def say(msg):
-    print(sc.api_call(
-      "chat.postMessage",
-      channel="#twitterautosearch",
-      text=msg
-    ))
-
-smart()
-#say("hi")
