055cc04089154fe883b11224a0bceaf8d81f1202
Greg Reinbach <greg@reinbach.com>
2017-09-11 06:39:38 -0400

    Add requirements and setup file

---
+++ setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+
+from setuptools import setup, find_packages
+
+VERSION = "0.1.0"
+HERE = os.path.dirname(__file__)
+
+
+def read(*files):
+    content = ''
+    for f in files:
+        content += codecs.open(os.path.join(HERE, f), 'r').read()
+    return content
+
+
+setup(
+    name='scraper',
+    url='https://github.com/CrowdRescueHQ/media-slack-scraper',
+    author='GrowdRescueHQ',
+    description='A bot that scrapes social media into slack.',
+    version=VERSION,
+    platforms=['any'],
+    license='MIT License',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['*.rst'],
+        'scraper': ['*'],
+    },
+    install_requires=read("requirements.txt"),
+)
